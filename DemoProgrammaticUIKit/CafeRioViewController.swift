27c309c4801a557000f18047eed0e8929df20c36
Aaron Martinez <aaronmar@zillow.com>
2023-02-08 15:39:30 -0700

    Build initial VC.

---
+++ DemoProgrammaticUIKit/CafeRioViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CafeRioViewController.swift
+//  DemoProgrammaticUIKit
+//
+//  Created by Aaron Martinez on 2/8/23.
+//
+
+import UIKit
+
+class CafeRioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
